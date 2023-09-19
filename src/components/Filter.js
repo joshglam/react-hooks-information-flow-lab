c9dc1ae3c9b07e6d715b58c2c37fae9f0541c2da
josh <jglam88@gmail.com>
2023-09-19 14:46:34 -0400

    finishes lab

---
+++ src/components/Filter.js
@@ -0,0 +1,12 @@
+export default function Filter({ onCategoryChange }) {
+    return (
+        <>
+            <select name="filter" onChange={onCategoryChange}>
+                <option value="All">Filter by category</option>
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Dessert">Dessert</option>
+            </select>        
+        </>
+    )
+}
